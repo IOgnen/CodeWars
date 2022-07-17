2cfba71b5d876bcd0d99084ecad8e5b8a8713ca3
IOgnen <ogneniloski@gmail.com>
2022-07-17 22:02:54 +0200

    Kata03

---
+++ src/YoureASquare.java
@@ -0,0 +1,27 @@
+public class YoureASquare {
+
+    public static void main(String[] args) {
+
+        System.out.println(isSquare(233068383));
+
+    }
+    public static boolean isSquare(int n) {
+
+        double number = 0;
+
+        try {
+            number = Math.sqrt(n);
+        } catch (Exception e) {
+            return false;
+        }
+
+        System.out.println(number);
+
+        if ((int)number*number == n ){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}

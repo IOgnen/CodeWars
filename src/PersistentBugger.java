34def9eda6d3a503b4545eca5e714b7257a61665
IOgnen <ogneniloski@gmail.com>
2022-07-19 12:24:47 +0200

    PersistentBugger

---
+++ src/PersistentBugger.java
@@ -0,0 +1,31 @@
+public class PersistentBugger {
+
+    public static void main(String[] args) {
+
+        persistentBugger(39);
+
+    }
+
+    private static int persistentBugger(long number) {
+
+        int counter = 0;
+
+        while(number > 9) {
+
+            long helper = number;
+            long proizvod = 1;
+
+            while(helper >= 1) {
+                proizvod = proizvod * (helper % 10);
+                helper = helper / 10;
+            }
+            counter++;
+            if (proizvod < 10) {
+                break;
+            }
+            number = proizvod;
+        }
+        return counter;
+    }
+
+}
